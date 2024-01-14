04ae579478fb53ce92f6f494d1f3405b9c8a9743
Simpi29 <sd623099@gmail.com>
2024-01-14 22:32:48 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,44 @@
+async function getSongs() {
+  let a = await fetch('/JS/spotifyClone/songs/');
+  let response = await a.text();
+
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  
+  for (let i = 0; i < as.length; i++) {
+    const element = as[i];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
+    }
+  }
+  
+  return songs;
+}
+
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
+
+  let songUL = document.querySelector('.songList').getElementsByTagName("ul")[0];
+
+  for (const song of songs) {
+    songUL.innerHTML += `<li>
+      <img class="invert" src="music.svg" alt="music">
+      <div class="songInfo">
+        <div class="songName">${song}</div>
+        <div class="songArtist">Simpi</div>
+      </div>
+      <div class="playNow">
+        <span>Play Now</span>
+        <img class="invert" src="play1.svg" alt="play1">
+      </div>
+    </li>`;
+  }
+
+  let audio = new Audio(songs[0]);
+  audio.play();
+}
+
+main();
